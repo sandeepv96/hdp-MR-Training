3ab0d79757c48630ccc98f0ada495879f715eb8b
Tomas <tomas.pica@gmail.com>
2014-01-04 13:40:02 -0500

    Fixing obvious bug where string was not converted to float for comparison

--- udacity_training/homework012/reducer.py
+++ udacity_training/homework012/reducer.py
@@ -5,13 +5,6 @@ import sys
 salesMax = 0
 oldKey = None
 
-# Loop around the data
-# It will be in the format key\tval
-# Where key is the store name, val is the sale amount
-#
-# All the sales for a particular store will be presented,
-# then the key will change and we'll be dealing with the next store
-
 for line in sys.stdin:
     data_mapped = line.strip().split("\t")
     if len(data_mapped) != 2:
@@ -26,7 +19,7 @@ for line in sys.stdin:
         salesMax = 0
 
     oldKey = thisKey
-    if thisSale > salesMax:
+    if float(thisSale) > salesMax:
     	salesMax = float(thisSale)
 
 if oldKey != None:
